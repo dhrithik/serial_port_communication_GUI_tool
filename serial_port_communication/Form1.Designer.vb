aab1065650b58d58d39467ba412cdc8f379ff56a
Dhrithik <Dhrithik@DHRITHIK-LAPTOP>
2024-06-26 21:23:12 +0530

    Add project files.

---
+++ serial_port_communication/Form1.Designer.vb
@@ -0,0 +1,210 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.ComboBoxBaudRate = New System.Windows.Forms.ComboBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.ComboBoxCOMPort = New System.Windows.Forms.ComboBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.RichTextBoxInput = New System.Windows.Forms.RichTextBox()
+        Me.RichTextBoxOutput = New System.Windows.Forms.RichTextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.ButtonWrite = New System.Windows.Forms.Button()
+        Me.ButtonStop = New System.Windows.Forms.Button()
+        Me.ButtonExit = New System.Windows.Forms.Button()
+        Me.ButtonStart = New System.Windows.Forms.Button()
+        Me.SerialPort1 = New System.IO.Ports.SerialPort(Me.components)
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Verdana", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(107, 26)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(617, 25)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "SERIAL PORT COMMUNICATION TESTING GUI TOOL"
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.ComboBoxBaudRate)
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Controls.Add(Me.ComboBoxCOMPort)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 79)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(807, 153)
+        Me.GroupBox1.TabIndex = 1
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Give Inputs"
+        '
+        'ComboBoxBaudRate
+        '
+        Me.ComboBoxBaudRate.FormattingEnabled = True
+        Me.ComboBoxBaudRate.Location = New System.Drawing.Point(588, 68)
+        Me.ComboBoxBaudRate.Name = "ComboBoxBaudRate"
+        Me.ComboBoxBaudRate.Size = New System.Drawing.Size(121, 21)
+        Me.ComboBoxBaudRate.TabIndex = 3
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(414, 72)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(91, 13)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Select Baud Rate"
+        '
+        'ComboBoxCOMPort
+        '
+        Me.ComboBoxCOMPort.FormattingEnabled = True
+        Me.ComboBoxCOMPort.Location = New System.Drawing.Point(135, 69)
+        Me.ComboBoxCOMPort.Name = "ComboBoxCOMPort"
+        Me.ComboBoxCOMPort.Size = New System.Drawing.Size(121, 21)
+        Me.ComboBoxCOMPort.TabIndex = 1
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(17, 69)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(86, 13)
+        Me.Label2.TabIndex = 0
+        Me.Label2.Text = "Select COM Port"
+        '
+        'RichTextBoxInput
+        '
+        Me.RichTextBoxInput.Location = New System.Drawing.Point(32, 286)
+        Me.RichTextBoxInput.Name = "RichTextBoxInput"
+        Me.RichTextBoxInput.Size = New System.Drawing.Size(354, 110)
+        Me.RichTextBoxInput.TabIndex = 2
+        Me.RichTextBoxInput.Text = ""
+        '
+        'RichTextBoxOutput
+        '
+        Me.RichTextBoxOutput.Location = New System.Drawing.Point(32, 435)
+        Me.RichTextBoxOutput.Name = "RichTextBoxOutput"
+        Me.RichTextBoxOutput.Size = New System.Drawing.Size(354, 110)
+        Me.RichTextBoxOutput.TabIndex = 3
+        Me.RichTextBoxOutput.Text = ""
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(29, 261)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(57, 13)
+        Me.Label4.TabIndex = 4
+        Me.Label4.Text = "Input Data"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(32, 416)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(79, 13)
+        Me.Label5.TabIndex = 5
+        Me.Label5.Text = "Recevied Data"
+        '
+        'ButtonWrite
+        '
+        Me.ButtonWrite.Location = New System.Drawing.Point(448, 348)
+        Me.ButtonWrite.Name = "ButtonWrite"
+        Me.ButtonWrite.Size = New System.Drawing.Size(96, 48)
+        Me.ButtonWrite.TabIndex = 6
+        Me.ButtonWrite.Text = "WRITE"
+        Me.ButtonWrite.UseVisualStyleBackColor = True
+        '
+        'ButtonStop
+        '
+        Me.ButtonStop.Location = New System.Drawing.Point(448, 435)
+        Me.ButtonStop.Name = "ButtonStop"
+        Me.ButtonStop.Size = New System.Drawing.Size(96, 48)
+        Me.ButtonStop.TabIndex = 7
+        Me.ButtonStop.Text = "STOP"
+        Me.ButtonStop.UseVisualStyleBackColor = True
+        '
+        'ButtonExit
+        '
+        Me.ButtonExit.Location = New System.Drawing.Point(708, 496)
+        Me.ButtonExit.Name = "ButtonExit"
+        Me.ButtonExit.Size = New System.Drawing.Size(96, 48)
+        Me.ButtonExit.TabIndex = 8
+        Me.ButtonExit.Text = "EXIT"
+        Me.ButtonExit.UseVisualStyleBackColor = True
+        '
+        'ButtonStart
+        '
+        Me.ButtonStart.Location = New System.Drawing.Point(448, 271)
+        Me.ButtonStart.Name = "ButtonStart"
+        Me.ButtonStart.Size = New System.Drawing.Size(96, 48)
+        Me.ButtonStart.TabIndex = 9
+        Me.ButtonStart.Text = "START"
+        Me.ButtonStart.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(831, 584)
+        Me.Controls.Add(Me.ButtonStart)
+        Me.Controls.Add(Me.ButtonExit)
+        Me.Controls.Add(Me.ButtonStop)
+        Me.Controls.Add(Me.ButtonWrite)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.RichTextBoxOutput)
+        Me.Controls.Add(Me.RichTextBoxInput)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Serial Port Communication GUI Tool"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents ComboBoxBaudRate As ComboBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents ComboBoxCOMPort As ComboBox
+    Friend WithEvents RichTextBoxInput As RichTextBox
+    Friend WithEvents RichTextBoxOutput As RichTextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents ButtonWrite As Button
+    Friend WithEvents ButtonStop As Button
+    Friend WithEvents ButtonExit As Button
+    Friend WithEvents ButtonStart As Button
+    Friend WithEvents SerialPort1 As IO.Ports.SerialPort
+End Class
---
+++ serial_port_communication/Form1.vb
@@ -0,0 +1,83 @@
+﻿Imports System.IO.Ports
+Imports System.Threading
+Imports System.IO
+
+Public Class Form1
+    Dim WithEvents serialPort As SerialPort
+    Dim rowIndex As Integer = 1
+    Dim readingThread As Thread
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ' Populate COM ports
+        For Each portName As String In SerialPort.GetPortNames()
+            ComboBoxCOMPort.Items.Add(portName)
+        Next
+
+        ' Add baud rate options
+        ComboBoxBaudRate.Items.AddRange({"9600", "19200", "38400", "57600", "115200"})
+
+        ' Set default baud rate selection if items are available
+        If ComboBoxBaudRate.Items.Count > 0 Then
+            ComboBoxBaudRate.SelectedIndex = 0
+        End If
+    End Sub
+
+    Private Sub ButtonStart_Click(sender As Object, e As EventArgs) Handles ButtonStart.Click
+        ' Initialize SerialPort
+        serialPort = New SerialPort(ComboBoxCOMPort.SelectedItem.ToString(), CInt(ComboBoxBaudRate.SelectedItem))
+        serialPort.Open()
+
+        ' Disable controls
+        ComboBoxCOMPort.Enabled = False
+        ComboBoxBaudRate.Enabled = False
+        ButtonStart.Enabled = False
+        ButtonStop.Enabled = True
+
+        ' Start reading thread
+        readingThread = New Thread(AddressOf ReadData)
+        readingThread.Start()
+    End Sub
+
+    Private Sub ButtonStop_Click(sender As Object, e As EventArgs) Handles ButtonStop.Click
+        ' Close serial port
+        serialPort.Close()
+
+        ' Enable controls
+        ComboBoxCOMPort.Enabled = True
+        ComboBoxBaudRate.Enabled = True
+        ButtonStart.Enabled = True
+        ButtonStop.Enabled = False
+
+        ' Clear rich text boxes
+        RichTextBoxInput.Clear()
+        RichTextBoxOutput.Clear()
+
+        ' Stop reading thread
+        If readingThread IsNot Nothing Then
+            readingThread.Abort()
+        End If
+    End Sub
+
+    Private Sub ButtonWrite_Click(sender As Object, e As EventArgs) Handles ButtonWrite.Click
+        ' Transmit data
+        serialPort.WriteLine(RichTextBoxInput.Text)
+    End Sub
+
+    Private Sub ReadData()
+        While serialPort.IsOpen
+            Try
+                ' Read data
+                Dim data As String = serialPort.ReadExisting
+
+                ' Display received data
+                Invoke(Sub() RichTextBoxOutput.AppendText(data & vbCrLf))
+            Catch ex As Exception
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            End Try
+        End While
+    End Sub
+
+    Private Sub ButtonExit_Click(sender As Object, e As EventArgs) Handles ButtonExit.Click
+        Close()
+    End Sub
+End Class
